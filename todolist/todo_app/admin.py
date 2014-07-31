fa87f55de9bdc98100649cc5188c9e4323ceac24
hstmage <jurik.tomas1@gmail.com>
2014-07-31 07:12:00 +0200

    todo_app,uprava modelu, pridanie app do admina

--- todolist/todo_app/admin.py
+++ todolist/todo_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from todo_app.models import Todo, Tags
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    fields = ['todo', 'author', 'date_cre']
+
+admin.site.register(Todo)
+admin.site.register(Tags)
--- todolist/todo_app/models.py
+++ todolist/todo_app/models.py
@@ -2,18 +2,21 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Tags(models.Model):
+    tag  = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.tag
+
 class Todo(models.Model):
     todo = models.CharField(max_length=200)
     date_cre = models.DateTimeField('date created')
     deleted = models.CharField(max_length=1)
     author = models.ForeignKey(User) 
+    tags = models.ManyToManyField(Tags)
     
     def __str__(self):
         return self.todo
-    
-class Tags(models.Model):
-    todo = models.ForeignKey(Todo)
-    tag  = models.CharField(max_length=30)
-    
-    def __str__(self):
-        return self.tag
+        
+    def is_deleted(self):
+        return self.deleted == '1'
